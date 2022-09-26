8284abef6fc340bfef775b09bb962fbbe641e0db
Jorge <jvascov@gmail.com>
2022-09-25 19:34:36 -0500

    Crear y listar propietarios

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+from routes.propietariosRoutes import create as createProp, read as readProp, update as updateProp
 
 app = Flask(__name__)
 
+cred = credentials.Certificate('./config/gKey.json')
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+
+@app.route('/propietarios', methods=['POST'])
+def crearPropietario():
+    collection_propietarios = db.collection('propietarios')
+    return createProp(collection_propietarios)
+
+@app.route('/propietarios', methods=['GET'])
+def leerPropietarios():
+    collection_propietarios = db.collection('propietarios')
+    return readProp(collection_propietarios)
+
+@app.route('/propietarios/<string:id>', methods=['GET', 'UPDATE'])
+def leerPropietario(id):
+    collection_propietarios = db.collection('propietarios')
+
+    if request.method == 'GET':
+        return readProp(collection_propietarios, id)
+    else:
+        return updateProp(collection_propietarios, id)
+
 port = int(os.environ.get('PORT', 8080))
 
-if __name__ == '__main':
-    app.run(threaded=True, host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port, debug=True)
---
+++ routes/propietariosRoutes.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+
+
+def create(collection):
+    try:
+        id = request.json['id']
+        
+        col = collection.document(id).get()
+
+        if not col:
+            collection.document(id).set(request.json)
+            return jsonify({'success': True}), 200
+        else:
+            return jsonify({'error': 'el usuario ya existe'}), 400
+    except Exception as e:
+        return f'Error registrando propietario: {e}'
+
+
+def read(collection, id = None):
+    try:
+        if id:
+            coll = collection.document(id).get()
+            return jsonify(coll.to_dict()), 200
+        else:
+            all_coll = [doc.to_dict() for doc in collection.stream()]
+            return jsonify(all_coll), 200
+    except Exception as e:
+        return f'Error leyendo propietarios: {e}'
+
+def update(collection, id):
+    try:
+        if id:
+            coll = collection.document(id).get()
+            return jsonify(coll.to_dict()), 200
+        else:
+            all_coll = [doc.to_dict() for doc in collection.stream()]
+            return jsonify(all_coll), 200
+    except Exception as e:
+        return f'Error leyendo propietarios: {e}'
